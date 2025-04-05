2f34ac6987e0c44fe03ca3d85ab714bb86efdee6
Shelby <snwilson0502@gmail.com>
2025-04-05 17:36:19 -0400

    more styling changes

--- game/pages/admin-page.tsx
+++ game/pages/admin-page.tsx
@@ -18,19 +18,19 @@ const AdminPage = () => {
 
 
     return (
-        <div className="flex flex-col w-full h-screen gap-20 p-8">
-          <header className="flex flex-col items-center w-full">
-            <h1 className="text-5xl font-medium inline-block">Category Game</h1>
-          </header>
-          <div className="flex flex-col gap-5 items-center">
-            <Link href={{ pathname: '/choose-num-players' }}>
-              <button className="btn btn-primary">
-                <div className="text-xl font-medium">Select Players</div>
-              </button>
-            </Link>
-          </div>
+      <div className="flex flex-col w-full h-screen gap-20 p-8">
+        <header className="flex flex-col items-center w-full">
+          <h1 className="text-5xl font-medium inline-block">Category Game</h1>
+        </header>
+        <div className="flex flex-col gap-5 items-center">
+          <Link href={{ pathname: '/choose-num-players' }}>
+            <button className="btn btn-primary">
+              <div className="text-xl font-medium">Select Players</div>
+            </button>
+          </Link>
         </div>
-      );
+      </div>
+    );
 }
 
 export default AdminPage;
